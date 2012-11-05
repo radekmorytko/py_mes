c8b4e01a4c5fd9fb833372642cb19272ec2da7eb
Kamil Figiela <kamil.figiela@gmail.com>
2012-11-06 00:15:43 +0100

    grayscale bitmap support

--- bmp_loader.py
+++ bmp_loader.py
@@ -7,8 +7,10 @@ def YCbCr(point):
     """
     Returns point value converted to YCbCr. Currently only 'Y' part is returned.
     """
-
-    return 0.299 * point[0] + 0.587 * point[1] + 0.114 * point[2]
+    if type(point) == int:
+        return point
+    else:
+        return 0.299 * point[0] + 0.587 * point[1] + 0.114 * point[2]
 
 class Bitmap:
     def __init__(self, filename):
@@ -18,7 +20,6 @@ class Bitmap:
     
     def as_matrix(self, convert_func = YCbCr):
         matrix = numpy.zeros((self.width, self.height))
-        print self.width, self.height
         for i in xrange(0, self.width):
             for j in xrange(0, self.height):
                 matrix[i,j] = convert_func(self.img.getpixel((i,j)))
