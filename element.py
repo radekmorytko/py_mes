bd836e0c22193990c3714962d945a5ccefcc4af5
Kamil Figiela <kamil.figiela@gmail.com>
2012-11-05 22:02:31 +0100

    align

--- element.py
+++ element.py
@@ -22,9 +22,9 @@ class Element:
     
     def calculate_b(self):
         a,b,c,d = self.a, self.b, self.c, self.d
-        f5 = lambda x: (self.mes.bitmap_derivative_x(x, c) - (self.coeff[0] * shift_me_baby(df_dx[0], a, b, c, d)(x, c) + self.coeff[1] * shift_me_baby(df_dx[1], a, b, c, d)(x, c) + self.coeff[2] * shift_me_baby(df_dx[2], a, b, c, d)(x, c) + self.coeff[3] * shift_me_baby(df_dx[3], a, b, c, d)(x, c))*shift_me_baby(functions[4], a, b, c, d)(x, c))
+        f5 = lambda x: (self.mes.bitmap_derivative_x(x, c) - (self.coeff[0] * shift_me_baby(df_dx[0], a, b, c, d)(x, c) + self.coeff[1] * shift_me_baby(df_dx[1], a, b, c, d)(x, c) + self.coeff[2] * shift_me_baby(df_dx[2], a, b, c, d)(x, c) + self.coeff[3] * shift_me_baby(df_dx[3], a, b, c, d)(x, c)) * shift_me_baby(functions[4], a, b, c, d)(x, c))
         f6 = lambda y: (self.mes.bitmap_derivative_y(b, y) - (self.coeff[0] * shift_me_baby(df_dy[0], a, b, c ,d)(b, y) + self.coeff[1] * shift_me_baby(df_dy[1], a, b, c, d)(b, y) + self.coeff[2] * shift_me_baby(df_dy[2], a, b, c, d)(b, y) + self.coeff[3] * shift_me_baby(df_dy[3], a, b, c, d)(b, y)) * shift_me_baby(functions[5], a, b, c, d)(b, y))
-        f7 = lambda x: (self.mes.bitmap_derivative_x(x, d) - (self.coeff[0] * shift_me_baby(df_dx[0], a, b, c, d)(x, d) + self.coeff[1] * shift_me_baby(df_dx[1], a, b, c, d)(x, d) + self.coeff[2] * shift_me_baby(df_dx[2], a, b, c, d)(x, d) + self.coeff[3] * shift_me_baby(df_dx[3], a, b, c, d)(x, d))*shift_me_baby(functions[6], a, b, c, d)(x, d))
+        f7 = lambda x: (self.mes.bitmap_derivative_x(x, d) - (self.coeff[0] * shift_me_baby(df_dx[0], a, b, c, d)(x, d) + self.coeff[1] * shift_me_baby(df_dx[1], a, b, c, d)(x, d) + self.coeff[2] * shift_me_baby(df_dx[2], a, b, c, d)(x, d) + self.coeff[3] * shift_me_baby(df_dx[3], a, b, c, d)(x, d)) * shift_me_baby(functions[6], a, b, c, d)(x, d))
         f8 = lambda y: (self.mes.bitmap_derivative_y(a, y) - (self.coeff[0] * shift_me_baby(df_dy[0], a, b, c ,d)(a, y) + self.coeff[1] * shift_me_baby(df_dy[1], a, b, c, d)(a, y) + self.coeff[2] * shift_me_baby(df_dy[2], a, b, c, d)(a, y) + self.coeff[3] * shift_me_baby(df_dy[3], a, b, c, d)(a, y)) * shift_me_baby(functions[7], a, b, c, d)(a, y))
 
         g5 = lambda x: shift_me_baby(df_dx[4], a, b, c, d)(x, c)**2
