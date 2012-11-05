a5d202100177f3df5f7f4dd1046493215549ccdf
kj <kjbch@wp.pl>
2012-11-05 22:25:37 +0100

    image size

--- bmp_loader.py
+++ bmp_loader.py
@@ -14,10 +14,11 @@ class Bitmap:
     def __init__(self, filename):
         self.img = Image.open(filename)
         self.img.load()
-        _, _, self.width, self.height = self.img.getbbox()
+        self.width, self.height = self.img.size
     
     def as_matrix(self, convert_func = YCbCr):
         matrix = numpy.zeros((self.width, self.height))
+        print self.width, self.height
         for i in xrange(0, self.width):
             for j in xrange(0, self.height):
                 matrix[i,j] = convert_func(self.img.getpixel((i,j)))
