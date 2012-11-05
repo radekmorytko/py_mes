d6c2b9f11cf01aac8db5b9befa5d0b45654bf8e1
Kamil Figiela <kamil.figiela@gmail.com>
2012-11-06 00:16:04 +0100

    another fixes

--- element.py
+++ element.py
@@ -5,7 +5,8 @@ from scipy.integrate import quadrature as quad
 from functions import shift_me_baby, df_dx, df_dy, functions
 
 class Element:
-    def __init__(self, mes, i, j):
+    def __init__(self, mes, i, j, iter):
+        self.iter = iter
         self.i, self.j = i, j
         self.mes = mes
         self.a, self.b, self.c, self.d = mes.ij_to_abcd(i,j)
@@ -13,7 +14,6 @@ class Element:
     
     def calculate_a(self):
         a,b,c,d = self.a, self.b, self.c, self.d
-        print a,b,c,d
         self.coeff[0] = self.mes.bitmap_value(a,c)
         self.coeff[1] = self.mes.bitmap_value(b,c)
         self.coeff[2] = self.mes.bitmap_value(b,d)
@@ -32,15 +32,15 @@ class Element:
         g7 = lambda x: shift_me_baby(df_dx[6], a, b, c, d)(x, d)**2
         g8 = lambda y: shift_me_baby(df_dy[7], a, b, c, d)(a, y)**2
         
-        q5u = quad(f5, a, b, vec_func=False, maxiter=100)[0]
-        q6u = quad(f6, c, d, vec_func=False, maxiter=100)[0]
-        q7u = quad(f7, a, b, vec_func=False, maxiter=100)[0]
-        q8u = quad(f8, c, d, vec_func=False, maxiter=100)[0]
-        
-        q5l = quad(g5, a, b, vec_func=False, maxiter=100)[0]
-        q6l = quad(g6, c, d, vec_func=False, maxiter=100)[0]
-        q7l = quad(g7, a, b, vec_func=False, maxiter=100)[0]
-        q8l = quad(g8, c, d, vec_func=False, maxiter=100)[0]
+        q5u = quad(f5, a, b, vec_func=False, maxiter=self.iter)[0]
+        q6u = quad(f6, c, d, vec_func=False, maxiter=self.iter)[0]
+        q7u = quad(f7, a, b, vec_func=False, maxiter=self.iter)[0]
+        q8u = quad(f8, c, d, vec_func=False, maxiter=self.iter)[0]
+
+        q5l = quad(g5, a, b, vec_func=False, maxiter=self.iter)[0]
+        q6l = quad(g6, c, d, vec_func=False, maxiter=self.iter)[0]
+        q7l = quad(g7, a, b, vec_func=False, maxiter=self.iter)[0]
+        q8l = quad(g8, c, d, vec_func=False, maxiter=self.iter)[0]
         
         self.coeff[4] = q5u/q5l
         self.coeff[5] = q6u/q6l
--- main.py
+++ main.py
@@ -14,22 +14,24 @@ if __name__ == '__main__':
     bmp_file = sys.argv[2]
     bitmap = Bitmap(bmp_file)
 
-    new_bitmap = numpy.matrix(numpy.zeros((n*16,n*16)))
+    new_bitmap = numpy.matrix(numpy.zeros((n*64,n*64)))
 
     mes = Mes(bitmap, n)
-    elements = [ [ Element(mes,i,j) for i in xrange(0,n) ] for j in xrange(0,n) ]
-    
+    elements = [ [ Element(mes,i,j,10) for i in xrange(0,n) ] for j in xrange(0,n) ]
+    i = 0
     for row in elements:
         for element in row:
+            i += 1
+            print i
             element.calculate_a()
             element.calculate_b()
             element.calculate_c()
 #            print element.u(0.5, 0.5)
 #            print mes.bitmap_value(element.i*mes.pixels_per_x, element.j*mes.pixels_per_y)
-            for x in xrange(0,16):
-                for y in xrange(0, 16):
-                    new_bitmap[element.i*16+x, element.j*16+y] = element.u(x/16.0, y/16.0)
+            for x in xrange(0,64):
+                for y in xrange(0, 64):
+                    new_bitmap[element.j*64+y, element.i*64+x] = element.u(x/64.0, y/64.0)
     plt.axis('off')
     plt.imshow(new_bitmap, cmap=plt.cm.gray)
     plt.show()
-    raw_input()
+#    raw_input()
