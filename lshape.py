9892374c90ff684c80fa3a128d6a35b13a74b506
Kamil Figiela <kamil.figiela@gmail.com>
2012-12-11 16:52:44 +0100

    zmiany

--- lshape.py
+++ lshape.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+# -*- encoding: utf-8 -*-
 from mes import Mes
 from functions import functions, shift_me_baby
 
@@ -11,6 +11,7 @@ class Element:
         self.xrange = xrange
         self.yrange = yrange
         self.coeff = [0] * 9
+        self.functions = map(lambda f: shift_me_baby(f, xrange[0], xrange[1], yrange[0], yrange[1]), functions)
 
     def u(self, i, j):
         assert xrange[0] <= i <= xrange[1], "Out of x range!"
@@ -19,10 +20,8 @@ class Element:
         if i < 0.5 and j < 0.5:
             return 0
 
-        return reduce(lambda x,y: x+shift_me_baby(y[0], xrange[0], 
-                                                  xrange[1], yrange[0], 
-                                                  yrange[1])(i,j)*y[1], 
-                      zip(functions, self.coeff), 0.0)
+        return reduce(lambda x,y: x+y[0](i,j)*y[1], 
+                      zip(self.functions, self.coeff), 0.0)
 
     def f(self, i, j):
         if i == 0 and j >= 0.5:
@@ -35,7 +34,7 @@ class Element:
 def global_u(elements, i, j):
     for row in elements:
         for element in row:
-            if element.xrange[0] <= i <= element.xrange[1] and
+            if element.xrange[0] <= i <= element.xrange[1] and \
                element.yrange[0] <= j <= element.yrange[1]:
                 return element.u
     
@@ -47,7 +46,7 @@ def laplacian(elements, i, j, dn):
 def bitmap(elements, i, j, delta, dn):
     for row in elements:
         for element in row:
-            if element.xrange[0] <= i <= element.xrange[1] and
+            if element.xrange[0] <= i <= element.xrange[1] and \
                element.yrange[0] <= j <= element.yrange[1]:
                 return element.u(i,j)+delta*laplacian(elements,i,j,dn)+delta*element.f(i,j)
 
@@ -75,4 +74,28 @@ if __name__ == '__main__':
                                           element.xrange[0], 
                                           element.yrange[0], 
                                           delta,
-                                          1.0/n
+                                          1.0/n)
+
+                f5 = lambda x: bitmap(element, x, element.yrange[0], delta, dn) - element.coeff[0] * element.functions[0](x, element.yrange[0])  - element.coeff[1] * element.functions[1](x, element.yrange[0]) - element.coeff[2] * element.functions[2](x, element.yrange[0]) - element.coeff[3] * element.functions[3](x, element.yrange[0])                
+                g5 = lambda x: elemet.functions[5]**2
+
+                f6 = lambda x: bitmap(element, element.xrange[1], x, delta, dn) - element.coeff[0] * element.functions[0](element.xrange[1], x)  - element.coeff[1] * element.functions[1](element.xrange[1], x)  - element.coeff[2] * element.functions[2](element.xrange[1], x)  - element.coeff[3] * element.functions[3](element.xrange[1], x) 
+                g6 = lambda x: elemet.functions[6]**2
+
+                f7 = lambda x: bitmap(element, x, element.yrange[1], delta, dn) - element.coeff[0] * element.functions[0](x, element.yrange[1])  - element.coeff[1] * element.functions[1](x, element.yrange[0]) - element.coeff[2] * element.functions[2](x, element.yrange[0]) - element.coeff[3] * element.functions[3](x, element.yrange[0])                
+                g7 = lambda x: elemet.functions[7]**2
+
+                f8 = lambda x: bitmap(element, element.xrange[0], x, delta, dn) - element.coeff[0] * element.functions[0](element.xrange[0], x)  - element.coeff[1] * element.functions[1](element.xrange[0], x)  - element.coeff[2] * element.functions[2](element.xrange[0], x)  - element.coeff[3] * element.functions[3](element.xrange[0], x) 
+                g8 = lambda x: elemet.functions[8]**2
+
+                q5u = quad(f5, element.xrange[0], element.xrange[1], vec_func=False, maxiter=self.iter)[0]
+                q5l = quad(f5, element.xrange[0], element.xrange[1], vec_func=False, maxiter=self.iter)[0]
+
+                q6u = quad(f6, element.yrange[0], element.yrange[1], vec_func=False, maxiter=self.iter)[0]
+                q6l = quad(f6, element.yrange[0], element.yrange[1], vec_func=False, maxiter=self.iter)[0]
+
+                q7u = quad(f7, element.xrange[0], element.xrange[1], vec_func=False, maxiter=self.iter)[0]
+                q7l = quad(f7, element.xrange[0], element.xrange[1], vec_func=False, maxiter=self.iter)[0]
+
+                q8u = quad(f8, element.yrange[0], element.yrange[1], vec_func=False, maxiter=self.iter)[0]
+                q8l = quad(f8, element.yrange[0], element.yrange[1], vec_func=False, maxiter=self.iter)[0]
